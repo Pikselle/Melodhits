310ad22081ef7cb18d964ff262874cce4fef8760
Pikselle <Maureen.metge@gmail.com>
2023-05-22 11:25:31 +0200

    Ajout logo accueil

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import dash_bootstrap_components as dbc
 
 bootstrap_theme = [dbc.themes.BOOTSTRAP, 'https://use.fontawesome.com/releases/v5.9.0/css/all.css']
 app = dash.Dash(__name__, external_stylesheets=bootstrap_theme)
-app.title = 'Melodhits'
+app.title = 'Melodhit'
 app._favicon = ("Logo-melodhit-noir.png")
 server = app.server
 app.config.suppress_callback_exceptions = True
